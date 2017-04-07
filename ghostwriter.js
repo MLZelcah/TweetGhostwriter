26cf1df86a5983d425ac7dca568af93e37465ffc
Unknown <mlewis46@mail.csuchico.edu>
2017-04-07 02:02:10 -0700

    Everything

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,20 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["What's with", "Have you ever heard about", "I had an idea about",
+   "I just want to talk about"];
+   var body = ["the sky", " the underground", "a great movie",
+    "Sutter", "aliens"];
+    var end = ["because it's weird", "too bad I can't remember it"];
+
+    randIndex = randomUpTo(intro.length - 1);
+    sentence = intro[randIndex];
+    randIndex = randomUpTo(body.length - 1);
+    sentence = sentence + " " + body[randIndex];
+    randIndex = randomUpTo(end.length - 1);
+    sentence = sentence + " " + end[randIndex];
+    document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
